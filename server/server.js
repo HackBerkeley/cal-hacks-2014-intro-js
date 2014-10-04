79d7e5677dbc58831505a90c9633797371d7938a
Brian Chu <bc@brianchu.com>
2014-10-04 05:17:08 -0700

    Cors tweak

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // cross origin requests
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
   next();
  });
 
