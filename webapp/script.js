ea9830ca958a1939910affa14587573f3a9d07af
Brian Chu <bc@brianchu.com>
2014-10-04 04:54:12 -0700

    Add post on client side

--- webapp/script.js
+++ webapp/script.js
@@ -1,8 +1,16 @@
+var updatePosts = function(post) {
+  var currentPosts = $('.posts');
+  var newPost = $('<div class="post">' + post.author + ': ' + post.msg + '</div>');
+  currentPosts.append(newPost);
+};
+
 var createFormHandler = function() {
   $('button').on('click', function(e) {
+    e.preventDefault();
     var author = $('input.author').val();
     var msg = $('input.msg').val();
-    debugger;
+    var post = {author:author, msg:msg};
+    updatePosts(post);
   });
 };
 
